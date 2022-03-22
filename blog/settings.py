fbb981f50cfc9f3edbdb4eee3bbc375334062211
Leandro0O <goncalvesleandro68@gmail.com>
2022-03-22 11:20:04 -0300

    att

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0gtgefu^=ttl+a*rdyb!k+o!c(hdytf&+w%zh!zat%hu*7u!m!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
