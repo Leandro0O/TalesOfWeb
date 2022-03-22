611f696f22bdbd436dcb930cc1df0f0927d4c0b0
Leandro Gonçalves <89206382+Leandro0O@users.noreply.github.com>
2022-03-22 06:23:33 -0300

    Delete posts directory

--- posts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import * 
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug','author','created','update')
-    prepopulated_fields = {"slug":("title",)}
-
-admin.site.register(Imagens)    
