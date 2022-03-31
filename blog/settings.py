2eaaf9b84cf81582fae155ad0be9f96b7d9285de
Leandro Gonçalves <89206382+Leandro0O@users.noreply.github.com>
2022-03-31 10:04:56 -0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #DEFAULT_FROM_EMAIL = 'talesofweb@gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'talesofweb@gmail.com'
-EMAIL_HOST_PASSWORD = 'lobomadotate314159'
+EMAIL_HOST_USER = '###'
+EMAIL_HOST_PASSWORD = '####'
 EMAIL_PORT = 587
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
